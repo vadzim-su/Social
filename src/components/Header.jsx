dabb5ecf0972b52b41afda8d2e1a0cabbffc7d99
vadzim-su <vadimmm0991@gmail.com>
2020-04-16 22:59:24 +0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="./logo.png" alt="" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="./panorama.jpg" alt="" />
+      </div>
+      <div>ava+description</div>
+      <div>My posts</div>
+      <div>New post</div>
+    </div>
+  );
+};
+
+export default Profile;
